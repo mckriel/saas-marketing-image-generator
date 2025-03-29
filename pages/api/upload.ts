2bac80f1e273907199283497580c1b39e2f2ed36
mckriel <mckriel@gmail.com>
2025-03-29 20:58:32 +0200

    tmpDir correction

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -20,7 +20,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(405).end('Method Not Allowed')
   }
 
-  const tmpDir = path.join(process.cwd(), 'tmp')
+  const tmpDir = '/tmp'
   if (!fs.existsSync(tmpDir)) {
     fs.mkdirSync(tmpDir)
   }
